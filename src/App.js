86e382b7336a57894f707367b8e5247ee0052649
Stian Kodehode <stiant.kodehode@gmail.com>
2022-08-22 13:49:28 +0200

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import {
+    StyledHeading,
+    ButtonContainer,
+    StyledButton,
+} from "./components/styledComponents";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [num, setNum] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setNum((oldNum) => oldNum + 1);
+        }, 1000);
+    }, []);
+
+    function changeNum(e) {
+        setNum((oldNum) => (e === "plus" ? oldNum + 1 : oldNum - 1));
+    }
+
+    return (
+        <div className="App">
+            <StyledHeading>{num}</StyledHeading>
+            <ButtonContainer>
+                <StyledButton
+                    onClick={() => {
+                        changeNum("plus");
+                    }}
+                >
+                    Add
+                </StyledButton>
+                <StyledButton onClick={changeNum}>Remove</StyledButton>
+            </ButtonContainer>
+        </div>
+    );
 }
 
 export default App;

6d87e727821d6dd822d0cccaa14e5e0b2ec5f17f
Stian Kodehode <stiant.kodehode@gmail.com>
2022-08-22 13:50:03 +0200

    first commit

---
+++ src/components/styledComponents.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledHeading = styled.h1`
+    color: white;
+    font-size: 40px;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    gap: 20px;
+`;
+
+export const StyledButton = styled.button`
+    padding: 1em;
+    min-width: 100px;
+    color: white;
+    background-color: blueviolet;
+    cursor: pointer;
+`;
